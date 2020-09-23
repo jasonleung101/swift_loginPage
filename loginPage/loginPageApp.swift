ba9c0a76ecddfd912a65bbebe8e3a32d925df790
jasonleung101 <jasonleung101@gmail.com>
2020-09-24 01:57:05 +0800

    Initial Commit

---
+++ loginPage/loginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginPageApp.swift
+//  loginPage
+//
+//  Created by Jason Leung on 24/9/2020.
+//
+
+import SwiftUI
+
+@main
+struct loginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
